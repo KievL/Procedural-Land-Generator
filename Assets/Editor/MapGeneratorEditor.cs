474cb75c72a88d4844caeb5491763223bb8ea41d
KievL <kiev.sider@hotmail.com>
2022-08-09 12:22:42 -0300

    Sync

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+[CustomEditor (typeof (MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator)target;
+
+        DrawDefaultInspector();
+
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.autoUpdate)
+            {
+                mapGen.GenerateMap();
+            }
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Procedural-Land-Generator
+Procedural Land Generator for Unity3D
